5ca3123512add2f19c7913be0cc3695477964853
mohammed imad umar <imadu1525@gmail.com>
2025-08-03 12:20:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Loader from "./components/Loader";
-import Footer from "./components/Footer";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
 import Dashboard from "./pages/Dashboard";
-
 import CGPACalculator from "./pages/CGPACalculator";
 import AssignmentTracker from "./pages/AssignmentTracker";
 import NotesApp from "./pages/NotesApp";
@@ -15,6 +12,7 @@ import SyllabusVault from "./pages/SyllabusVault";
 import AttendanceTracker from "./pages/AttendanceTracker";
 import TimeTablePlanner from "./pages/TimeTablePlanner";
 import StickyNotes from "./pages/StickyNotes";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -26,11 +24,11 @@ export default function App() {
         <Route path="/cgpa" element={<CGPACalculator />} />
         <Route path="/assignments" element={<AssignmentTracker />} />
         <Route path="/notes" element={<NotesApp />} />
-        <Route path="/classlinks" element={<ClassLinks />} />
+        <Route path="/class-links" element={<ClassLinks />} />
         <Route path="/syllabus" element={<SyllabusVault />} />
         <Route path="/attendance" element={<AttendanceTracker />} />
         <Route path="/timetable" element={<TimeTablePlanner />} />
-        <Route path="/stickynotes" element={<StickyNotes />} />
+        <Route path="/sticky-notes" element={<StickyNotes />} />
       </Routes>
       <Footer />
     </Router>
