1bf8f264125d17a8ad52cb8ec850472c680b07cf
mohammed imad umar <imadu1525@gmail.com>
2025-08-03 13:32:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Auth
 import Login from "./auth/Login";
 import Signup from "./auth/Signup";
-
-// Pages & Features
 import Dashboard from "./pages/Dashboard";
 import CGPACalculator from "./pages/CGPACalculator";
 import AssignmentTracker from "./pages/AssignmentTracker";
@@ -15,27 +13,28 @@ import SyllabusVault from "./pages/SyllabusVault";
 import AttendanceTracker from "./pages/AttendanceTracker";
 import TimeTablePlanner from "./pages/TimeTablePlanner";
 import StickyNotes from "./pages/StickyNotes";
-
-// Footer
 import Footer from "./components/Footer";
+import { ThemeProvider } from "./context/ThemeContext";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/cgpa" element={<CGPACalculator />} />
-        <Route path="/assignments" element={<AssignmentTracker />} />
-        <Route path="/notes" element={<NotesApp />} />
-        <Route path="/class-links" element={<ClassLinks />} />
-        <Route path="/syllabus" element={<SyllabusVault />} />
-        <Route path="/attendance" element={<AttendanceTracker />} />
-        <Route path="/timetable" element={<TimeTablePlanner />} />
-        <Route path="/sticky-notes" element={<StickyNotes />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cgpa" element={<CGPACalculator />} />
+          <Route path="/assignments" element={<AssignmentTracker />} />
+          <Route path="/notes" element={<NotesApp />} />
+          <Route path="/class-links" element={<ClassLinks />} />
+          <Route path="/syllabus" element={<SyllabusVault />} />
+          <Route path="/attendance" element={<AttendanceTracker />} />
+          <Route path="/timetable" element={<TimeTablePlanner />} />
+          <Route path="/sticky-notes" element={<StickyNotes />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   );
 }
